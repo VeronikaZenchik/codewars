48cc7b01a46cce51505c913618dbecd5f11819a2
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-08-02 00:56:20 +0300

    init project

---
+++ 8 kyu/reversed-strings.js
@@ -0,0 +1,16 @@
+// Description:
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+//SOLUTION:
+function solution(str){
+  return str.split('').reverse().join('');
+}
+
+
+
+
+
+
