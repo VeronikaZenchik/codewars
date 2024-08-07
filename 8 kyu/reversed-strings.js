560fe5c5226e86b7d2c22c03b53819c39d6a1ec3
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-08-07 23:15:06 +0300

    reversed-strings for

--- 8 kyu/reversed-strings.js
+++ 8 kyu/reversed-strings.js
@@ -7,8 +7,20 @@
 //SOLUTION:
 
 function solution(str){
-  return str.split('').reverse().join('');
+  let newString = '';
+  for(let i = str.length - 1; i >= 0; i--) {
+    newString += str[i]
+  }
+  return newString
 }
+console.log(solution('bear'));
+
+
+// решение через метод split
+
+// function solution(str){
+//   return str.split('').reverse().join('');
+// }
 
 
 
