8b218c5974589fd62ef135a8b0ea5dc78518268e
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-09-16 18:56:01 +0300

    methods number

---
+++ 8 kyu/methods-of-string-object-slice-substring-and-subst.js
@@ -0,0 +1,35 @@
+// Task
+// Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+// The first mission: Traversing arr, find the shortest string length.
+
+// The second mission: Traversing arr again, intercept all strings to the shortest string length(Start from index0). you can use one of slice() substring() or substr() do it. return the result after finished the work.
+
+// for example:
+
+// cutIt(["ab","cde","fgh"]) should return ["ab","cd","fg"]
+// cutIt(["abc","defgh","ijklmn"]) should return ["abc","def","ijk"]
+// cutIt(["codewars","javascript","java"]) should return ["code","java","java"
+
+
+// SOLUTION:
+
+function cutIt(arr) {
+  let minLength = Infinity;
+  for (let str of arr) {
+      if (str.length < minLength) {
+          minLength = str.length;
+      }
+  }
+  let result = [];
+  for (let str of arr) {
+      result.push(str.slice(0, minLength));
+  }
+  return result;
+}
+
+
+console.log(cutIt(["ab", "cde", "fgh"])); // ["ab", "cd", "fg"]
+console.log(cutIt(["abc", "defgh", "ijklmn"])); // ["abc", "def", "ijk"]
+console.log(cutIt(["codewars", "javascript", "java"])); // ["code", "java", "java"]
+
